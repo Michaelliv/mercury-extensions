15c684f1b3fe924ecdd95f57698be7e105a6ad65
Michael Livshits <livsmichael@gmail.com>
2026-03-18 15:11:11 +0200

    fix: simplify github cli install

--- packages/github/index.ts
+++ packages/github/index.ts
@@ -3,7 +3,7 @@ export default function (mercury: MercuryExtensionAPI) {
   mercury.cli({
     name: "gh",
     install:
-      "curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg && chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg && echo 'deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main' > /etc/apt/sources.list.d/github-cli.list && apt-get update && apt-get install -y --no-install-recommends gh && rm -rf /var/lib/apt/lists/*",
+      "apt-get update && apt-get install -y --no-install-recommends gh && rm -rf /var/lib/apt/lists/*",
   });
 
   mercury.permission({ defaultRoles: ["admin"] });
