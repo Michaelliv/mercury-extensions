3265933c8c2d51d2cd11ed3ab51ca34c77048ff7
Michael Livshits <livsmichael@gmail.com>
2026-03-18 15:04:53 +0200

    chore: update github extension

--- packages/github/index.ts
+++ packages/github/index.ts
@@ -3,7 +3,7 @@ export default function (mercury: MercuryExtensionAPI) {
   mercury.cli({
     name: "gh",
     install:
-      "apt-get update && apt-get install -y --no-install-recommends gh git && rm -rf /var/lib/apt/lists/*",
+      "curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg && chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg && echo 'deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main' > /etc/apt/sources.list.d/github-cli.list && apt-get update && apt-get install -y --no-install-recommends gh && rm -rf /var/lib/apt/lists/*",
   });
 
   mercury.permission({ defaultRoles: ["admin"] });
